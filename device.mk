98d090acec7100be952628522be9f4a04ff82ceb
kondors1995 <normandija1945@gmail.com>
2025-06-19 12:12:40 +0000

    raphael: Move camera Flags to soong config
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- device.mk
+++ device.mk
@@ -199,6 +199,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
+$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,camera,package_name,com.android.camera)
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.raphael)
+
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
