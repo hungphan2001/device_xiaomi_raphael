2dae4400668dd5d65b59b06224e3ef7827d3fdcd
Hung Phan <phandinhhungvp2001@gmail.com>
2025-07-25 02:43:12 +0000

    raphael: go cherish
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/cherish/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/hidl/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
